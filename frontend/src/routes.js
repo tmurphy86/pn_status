3547eac2d225b5dcec86f30e61d8e0055ef67870
Tim Murphy <tim.murphy86@gmail.com>
2018-01-25 17:50:03 -0500

    router page

---
+++ frontend/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch} from "react-router-dom";
+
+
+const routes = (
+  <Switch>
+    <Route path="/" exact component= {App} />
+  </Switch>
+
+)
+
+export default routes;
